cd235995a62b1eb6720532de8109f7da9e34c5b4
Philippe Ombredanne <pombredanne@nexb.com>
2021-03-25 14:27:29 +0100

    Add proper deprecated legacy debut module
    This is strictly for short term backward compat.
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ src/debut/debcon.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) nexB Inc. and others.
+# http://nexb.com and https://github.com/nexB/debian_inspector/
+
+# SPDX-License-Identifier: Apache-2.0
+
+# legacy module for backward compat
+import warnings
+warnings.warn("debut module is deprecated. Use debian_inspector instead.", DeprecationWarning, stacklevel=1)
+from debian_inspector.debcon import *
